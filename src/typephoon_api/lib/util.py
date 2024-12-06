ee178aa2bac364cc4274a1e27342c2800ca2e012
AlexFangSW <alexfangsw@gmail.com>
2024-12-06 10:06:01 +0800

    chore: sanitize dsn in logs

--- src/typephoon_api/lib/util.py
+++ src/typephoon_api/lib/util.py
@@ -2,14 +2,28 @@ from logging import getLogger
 from logging.config import dictConfig
 from alembic import command
 from alembic.config import Config
+from pydantic_core import Url
 
 from ..types.setting import TRACE, Setting
 
 logger = getLogger(__name__)
 
 
+def sanitized_dsn(dsn: str) -> Url:
+    temp_url = Url(dsn)
+    assert temp_url.host
+    assert temp_url.path
+    url = Url.build(scheme=temp_url.scheme,
+                    username=temp_url.username,
+                    password="***",
+                    host=temp_url.host,
+                    port=temp_url.port,
+                    path=temp_url.path.lstrip("/"))
+    return url
+
+
 def db_migration(setting: Setting):
-    logger.info("running migration, DSN: %s", setting.db.dsn)
+    logger.info("running migration on %s", sanitized_dsn(setting.db.dsn))
     config = Config()
     config.set_main_option("script_location", "migration")
     config.set_main_option("sqlalchemy.url", setting.db.dsn)
